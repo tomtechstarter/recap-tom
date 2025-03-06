70b3de96cc332d15c30141435aa3a59a6cfe8d19
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2025-03-06 12:14:42 +0100

    add task gitdocu

---
+++ gitdocu.js
@@ -0,0 +1,6 @@
+const fetchGitWorkflowImage = async () => {
+  const imagePath = "";
+  const gitDocuContainer = undefined;
+  // Image in den gitDocuContainer einfüge (TIPP: .innerHTML)
+  // Das Image sol einen vordefinierten Style hinterlegt
+};
